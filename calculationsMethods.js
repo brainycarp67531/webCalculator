245393d35c2c6a8a7e2ef8b5c92b7ef841cc194f
Björn Sterling <bjorn.sterling@gmail.com>
2025-09-02 09:05:47 +0200

    splited the js file, updated the calculation event lisnter to handle more operations

---
+++ calculationsMethods.js
@@ -0,0 +1,21 @@
+// function for adding two numbers
+function add(number1, number2) {
+    return number1 + number2;
+};
+
+
+// function for subtracting two numbers
+function subtract(number1, number2) {
+    return number1 - number2;
+};
+
+
+//function for multiply two numbers
+function multiply(number1, number2) {
+    return number1 * number2;
+};
+
+// function for dividing two numbers. 
+function divide(number1, number2) {
+    return number1 / number2;
+};
--- js-file.js
+++ js-file.js
@@ -1,25 +1,3 @@
-// function for adding two numbers
-function add(number1, number2) {
-    return number1 + number2;
-};
-
-
-// function for subtracting two numbers
-function subtract(number1, number2) {
-    return number1 - number2;
-};
-
-
-//function for multiply two numbers
-function multiply(number1, number2) {
-    return number1 * number2;
-};
-
-// function for dividing two numbers. 
-function divide(number1, number2) {
-    return number1 / number2;
-};
-
 
 // function that takes two number and one operation and calls the correct function to correct function to the the operation. 
 function operate(number1, number2, operator) {
@@ -159,92 +137,10 @@ enter.addEventListener("click", () => {
 
     // start the function for doing the calculation
     
-    // Check for multiplications via a loop over the calculation array
-    for (let i = 0; i < calculation.length; i++) {
-
-        // variables to keep track of what to replace in the initial array. Will be used when spliceing the calculation array. 
-        let startIndex = 0;
-        let deleteCount = 0;
-        // variable to keep track of the result 
-        let result = 0;
-
-        // and store the multiplication sign in the variable operation. 
-        if (calculation[i] === "*") {
-            operation = calculation[i];
-            console.log("operator is stored.")
-            console.log(operation);
-            console.log("i");
-            console.log(i);
-            console.log(typeof(calculation[0]));
-            console.log(typeof(calculation[i]));
-
-
-            // multiple the two numbers before and after the multiplication sign
-            // check before the sign and store the first number in the variable "number1"
-            for (let j = 1; i - j >= 0; j++) {
-                if (calculation[i-j] >= "0" && calculation[i-j] <= "9") {
-                    console.log("testing for a number 1 at i =", i-j);
-                    if (i-j == 0) {
-                        // store the number before the * sign in the variable "number1".
-                        number1 = parseInt(calculation.slice(0, i).join(""));
-                        console.log("number1", number1);
-                        startIndex = 0;
-                        break;
-                    }
-                    continue;
-                } else {
-                    // store the number before the * sign in the variable "number1".
-                    number1 = parseInt(calculation.slice(i - j + 1, i).join(""));
-                    console.log("number1", number1);
-                    startIndex = (i - j + 1);
-                    console.log("startIndex: ", startIndex);
-                    break;
-                };
-            };
-
-
-            // check after the sign and store the second number in the variable "number2"
-            for (let k = 1; i + k < calculation.length; k++) {
-                if (calculation[i+k] >= "0" && calculation[i+k] <= "9") {
-                    console.log("testing for a number 2 at i = ", i+k);
-                    if (i+k == calculation.length - 1) {
-                        number2 = parseInt(calculation.slice(i + 1).join(""));
-                        console.log("number2", number2);
-                        deleteCount = calculation.length - 1 - startIndex;
-                        break;
-                    }
-                    continue;
-                }else {
-                    // store the number before the * sign in the variable "number1".
-                    number2 = parseInt(calculation.slice(i + 1, i + k).join(""));
-                    console.log("number2", number2);
-                    deleteCount = i + k - startIndex;
-                    console.log("deleteCount: ", deleteCount);
-                    break;
-                };
-            };
-
-            
-            // check if number1 and number2 is greater then Zero 
-            if (number1 > 0 && number2 > 0) {
-                // calculate the result
-                result = operate(number1, number2, "m");
-                console.log("result: ", result);
-                
-                // Make the result in to and array 
-                result = result.toString().split("").map(Number);
-                console.log("result as an array: ", result);
-
-                // use slice to insert the calculate value in the initial array and replace the calcualtion. 
-                calculation.splice(startIndex, deleteCount, ...result);
-                console.log("the updated calculation array: ", calculation);
-
-            }
-
-        }
-
-    }
-
+    calculation = doTheCalculation(calculation, "*")
+    calculation = doTheCalculation(calculation, "/")
+    calculation = doTheCalculation(calculation, "+")
+    calculation = doTheCalculation(calculation, "-")
 
     updateDisplay();
 
